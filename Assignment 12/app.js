a542264098b2b3f0aedb5051eb43b74cb5d7cd4d
Mirza Hammad Baig <hammadsajid807@gmail.com>
2026-01-05 04:04:17 +0500

    Javascript first assignment

---
+++ Assignment 12/app.js
@@ -0,0 +1,53 @@
+// ============================
+// Chapter 02 - Variables for Strings
+// ============================
+
+// Q1: Declare a variable called username
+let username;
+document.write("<b>Q1:</b> Variable 'username' declared.<br><br>");
+
+// Q2: Declare a variable called myName and assign your full name
+let myName = "Mirza Hammad Baig";
+document.write("<b>Q2:</b> My name is: " + myName + "<br><br>");
+
+// Q3: Declare a variable titled message
+let message;
+document.write("<b>Q3:</b> Variable 'message' declared.<br><br>");
+
+// Q4: Assign “Hello World” to the variable message and alert it
+message = "Hello World";
+alert(message);
+document.write("<b>Q4:</b> Message assigned and alerted: " + message + "<br><br>");
+
+// Q5: Save student’s bio data in variables and show in alerts
+let studentName = "Hammad";
+let studentAge = "22 years old";
+let studentCourse = "Web & Mobile Application Development";
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+document.write("<b>Q5:</b><br>");
+document.write("Student Name: " + studentName + "<br>");
+document.write("Student Age: " + studentAge + "<br>");
+document.write("Course: " + studentCourse + "<br><br>");
+
+// Q6: Display message in different lines
+let pizza = "PIZZA<br>PIZZ<br>PIZ<br>PI<br>P";
+document.write("<b>Q6:</b><br>" + pizza + "<br><br>");
+
+// Q7: Declare email variable & store a message
+let email = "mirzahammadbaig543@gmail.com";
+document.write("<b>Q7:</b> My email address is: " + email + "<br><br>");
+
+// Q8: Store a book name in a variable
+let book = "A smarter way to learn JavaScript";
+document.write("<b>Q8:</b> I am trying to learn from the book: " + book + "<br><br>");
+
+// Q9: Write a script to display this message in browser
+document.write("<b>Q9:</b> Yah! I can write HTML content through JavaScript<br><br>");
+
+// Q10: Store a string in a variable and show it
+let design = "▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬";
+document.write("<b>Q10:</b> " + design + "<br><br>");
