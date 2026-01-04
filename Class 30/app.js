71eb6dd1d08452128255a734ec6ddad3f8ec4dc3
Mirza Hammad Baig <hammadsajid807@gmail.com>
2026-01-05 04:22:27 +0500

    Add files via upload

---
+++ Class 30/app.js
@@ -0,0 +1,30 @@
+//  var totalCost1 = 1 + (3 * 4);
+// var totalCost2 = (1 + 3) * 4;
+// document.write(totalCost1 + " " +  totalCost2)
+
+
+// var name = prompt('enter your name');
+// var phone= prompt('enter your phone number');
+
+// console.log(name);
+
+
+// var num1= +prompt('enter first number ');
+// var num2= +prompt('enter second number ');
+
+// document.write(Number(num1) + Number(num2))
+
+//  var x = prompt("Where does the Pope live?");
+//   if (x === "Vatican") {
+//     alert("Correct!");
+//   }
+
+var x = prompt ("Where does the Pope live?");
+
+if(x==="Vatican")
+{
+    alert("Correct");
+}
+else{
+    alert("Wrong Answer");
+}
