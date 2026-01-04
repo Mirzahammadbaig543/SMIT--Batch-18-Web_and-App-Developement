ff2f9907005e163cdeb17b1c6e59596310ddd390
Mirza Hammad Baig <hammadsajid807@gmail.com>
2026-01-05 04:23:10 +0500

    Add files via upload

---
+++ Class 31/app.js
@@ -0,0 +1,43 @@
+// if else
+//     let isHungry= true;
+
+// if (isHungry){
+//     console.log("let's eat!");
+
+// }
+// else {
+//     console.log("Maybe Later...");
+// }
+
+
+// primitve data Types
+// 1.'String'
+// 2.'Number'
+// 3.'undefined'
+// 4. Null
+
+// Non primitve data Types
+
+var myName = undefined
+var myName = null
+let name1 = undefined
+name1 = ''
+var name3 = 0
+const war = America;
+// war = 12;
+// let myWorld;
+// myWorld = 'it is';
+
+var war2 =12;
+if(war2 == '12'){
+    alert('yes it is')
+}
+else{
+    alert('not')
+}
+
+///////// var ka concept iske keyword se agar ham variable declare krte to same var se ham isko dubare declare krskte hein
+
+////////////let me same variable name se dubara declare nh krskte lkn value assign krskte hein bd me  /////////////
+
+///////////// const me ye hota hai ke agar isko use krte hein to usi time value assign krni paregi /////////////
