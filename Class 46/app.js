4f6a8b4be9806b63d79b55583197d510b2fb9ab2
Mirza Hammad Baig <hammadsajid807@gmail.com>
2026-01-05 03:56:56 +0500

    Add files via upload

---
+++ Class 46/app.js
@@ -0,0 +1,63 @@
+// var obj1 = {
+//     stdName : 'Subhan',
+//     rollNum : 12,
+//     class : 'five',
+//     age : 10,
+
+// }
+
+
+
+
+
+
+// for (var key in obj1 ){
+//     document.write(key + ':' + obj1[key] + '<br>' );
+
+// }
+// var name = obj1.stdName;
+// head.innerHtml = name 
+
+
+// var obj1 = {
+//     stdName : 'Hammad',
+//     rollNum : 123,
+//     class : 'five',
+//     age : 10,
+
+// }
+
+// var newStd = 'Ali';
+
+// obj1.stdName = newStd;
+// console.log(obj1);
+
+
+// var obj.plan1 = {
+//     name : 'Basic',
+//     price : 3.99,
+//     space : 100,
+//     transfer : 1000,
+//     pages : 10,
+
+// }
+
+// var obj.plan2 = {
+//     name : 'Professional',
+//     price : 5.99,
+//     space : 500,
+//     transfer : 5000,
+//     pages : 50,
+
+// }
+
+// var obj.plan3 = {
+//     name : 'Ultimate',
+//     price : 9.99,
+//     space : 2000,
+//     transfer : 20000,
+//     pages : 500,
+
+// }
+// console.log(obj.plan1)
+// // plan1.innerHtml = name
