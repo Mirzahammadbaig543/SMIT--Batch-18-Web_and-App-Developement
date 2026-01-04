79a01d644757b44e3e9134b1aaf5ac57df8754a1
Mirza Hammad Baig <hammadsajid807@gmail.com>
2026-01-05 04:17:07 +0500

    Add files via upload

---
+++ Class 38/app.js
@@ -0,0 +1,43 @@
+// var Countries = [ 'Pakistan' , 'Bangladesh ', 'England'];
+// console.log('Before Method', Countries );
+
+
+// var newCountries = ['London ', 'New York']
+// Countries.splice(Countries.length , 0 , newCountries )  
+// console.log('After Method', Countries );
+
+// var countries = [ 'Pakistan' , 'Bangladesh ', 'England' , 'London ', 'New York'];
+
+// // var newArr = countries.slice(1,4)
+// // console.log('new array', newArr)   // jo isko limit dete hein us se ek index pehle end krta hai means
+// //                                        // jese 4 duga to show 3 tk hoga
+ 
+// var newArray = countries.slice(3) 
+
+// console.log('New ', newArray)
+
+// console.log('countries', countries)
+
+//Function
+function alertUser (name , greet){
+    // var userName = prompt('Username');
+    alert(`Assalam Alaikum ${name}`)
+    alert(`${greet}`)
+
+};
+
+// alertUser();
+
+document.write('Hello ' + '<br>')
+
+document.write('Where are you ' + '<br>')
+
+
+document.write('why you are not looking here' + '<br>')
+
+document.write('Look here' + '<br>')
+
+var name1 = 'Saylani';
+var greet = "How are you?"
+alertUser(name1 , greet);
+                                         
