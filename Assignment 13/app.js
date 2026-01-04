06ce3239b4040ed1f4b4bb49c3cefd733ad58cdf
Mirza Hammad Baig <hammadsajid807@gmail.com>
2026-01-05 04:05:31 +0500

    Variable for Numbers Exercise

---
+++ Assignment 13/app.js
@@ -0,0 +1,22 @@
+// Q1: Declare age variable and show in alert
+let age = 22;
+alert("I am " + age + " years old");
+document.write("<b>Q1:</b> I am " + age + " years old<br><br>");
+
+// Q2: How many times a visitor has visited—simulate number
+let visitCount = 14;
+alert("You have visited this site " + visitCount + " times");
+document.write("<b>Q2:</b> You have visited this site " + visitCount + " times<br><br>");
+
+// Q3: Birth year variable
+let birthYear = 2002;
+document.write("<b>Q3:</b> My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is number<br><br>");
+
+// Q4: Online clothing store message
+let visitorName = "Hammad";
+let productTitle = "T-shirt(s)";
+let quantity = 5;
+
+document.write("<b>Q4:</b><br>");
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing Store.<br><br>");
