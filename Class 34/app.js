aa37b084855298ab980df974d9d08a069d72bc3b
Mirza Hammad Baig <hammadsajid807@gmail.com>
2026-01-05 04:25:25 +0500

    Add files via upload

---
+++ Class 34/app.js
@@ -0,0 +1,51 @@
+// var player = prompt('from which city you are');
+
+// var city = "Karachi" ;
+
+// var city = "Multan" ;
+// if(player === city){
+//    alert ('you can come')
+// }
+// else{
+//     alert ( 'You are not allowed')
+// }
+
+//  var user = prompt('from which city you are');
+//  var score = 0;
+//  var userIQ = "not available";
+//  var city= "Karachi";
+//  if (user === city){
+//      score++;
+//      userIQ="genius";
+//      document.write(userIQ + '<br>'  );
+//      document.write(score);
+//      alert("You can come");
+//  }
+//  else {
+//     alert ('you are not allowed');
+//  }
+
+// var x = 10;
+// var y = 10;
+// var a = 40;
+// var b = 20;
+// var b = 40;
+
+// if ( x + y === a-b)[
+//     alert( 'yes it is true')
+// ]
+
+// if ( x + y !== a-b)[
+//     alert( 'yes it is false')
+// ]
+
+var age = 30;
+var correction = 'Age is Correct';
+var Incorrect = ' Your Age is wrong ';
+if (age < 20)
+{
+    alert(correction);
+}
+else {
+    alert (Incorrect);
+}
