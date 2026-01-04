b81ea4a64207817ce39266b47f4dc4ef2ab29908
Mirza Hammad Baig <hammadsajid807@gmail.com>
2026-01-05 04:21:34 +0500

    Add files via upload

---
+++ Class 29/app.js
@@ -0,0 +1,13 @@
+var myName;
+var num1;
+
+var firstName="New";
+
+var lastName="World";
+var num1 ='1';
+var num2 ='2';
+
+document.write( firstName + " " + lastName )
+// console.log(firstName+lastName)
+// console.log(num1)
+// console.log(num1-num2);
